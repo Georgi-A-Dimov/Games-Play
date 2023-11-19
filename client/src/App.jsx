f623375f4cc812a4369b8eaa77a88d1b3185ff6c
Georgi-A-Dimov <122820016+Georgi-A-Dimov@users.noreply.github.com>
2023-11-19 15:32:14 +0200

    Added react router library
    Add Header
    Add Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
     return (
         <div id="box">
+            <Header />
             
+            <Routes>
+                <Route path='/' element={<Home/>} />
+            </Routes>
         </div>
     )
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            {/* <!-- Navigation --> */}
+            <h1><Link className="home" to="/">GamesPlay</Link></h1>
+            <nav>
+                <Link to="#">All games</Link>
+                {/* <!-- Logged-in users --> */}
+                <div id="user">
+                    <Link to="#">Create Game</Link>
+                    <Link to="#">Logout</Link>
+                </div>
+                {/* <!-- Guest users --> */}
+                <div id="guest">
+                    <Link to="#">Login</Link>
+                    <Link to="#">Register</Link>
+                </div>
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
