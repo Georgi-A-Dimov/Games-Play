9c221adf90d05a6878982d4527cbed8bc7ca1f2d
Georgi-A-Dimov <122820016+Georgi-A-Dimov@users.noreply.github.com>
2023-11-26 17:08:57 +0200

    Created useForm
    Used useForm for login.
    Added AuthContext.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
+
+import AuthContext from './contexts/AuthContext';
+
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import GameList from './components/game-list/GameList';
@@ -8,18 +11,23 @@ import CreateGame from './components/create-game/CreateGame';
 import GameDetails from './components/game-details/GameDetails';
 
 function App() {
+    const loginSubmitHadler = () => {
+
+    };
+    
     return (
         <div id="box">
             <Header />
-
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/games' element={<GameList />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/create-game' element={<CreateGame />} />
-                <Route path='/games/:gameId' element={<GameDetails />} />
-            </Routes>
+            <AuthContext.Provider value={loginSubmitHadler}>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/games' element={<GameList />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create-game' element={<CreateGame />} />
+                    <Route path='/games/:gameId' element={<GameDetails />} />
+                </Routes>
+            </AuthContext.Provider>
         </div>
     )
 }
